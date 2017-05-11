09b980aaa8f2c998f64344b3d49ee03c04b78ebf
Dale 'Ducky' Lotts <dlotts@knightrider.com>
2017-05-11 09:37:38 -0500

    fix(vanilla): fix minor issues in original implementation
    Removed onclick handlers, moved main into body so increment and decrement elements exist, and change
    
    iife syntax to fully encapsulate the function

--- Redux/REF_Vanilla/main.js
+++ Redux/REF_Vanilla/main.js
@@ -1,59 +1,61 @@
-(function(){
-
-    // Reducer
-    //  (state, action) => state
-    //      INCREMENT ? state + 1
-    //      DECREMENT ? state - 1
-    var counterReducer = function(state, action) {
-        // INITIAL STATE
-        if(state == undefined){
-            return 0;
-        }
-
-        // return new state based on actions
-        switch(action.type) {
-            case 'INCREMENT':
-                return state + 1;
-            
-            case 'DECREMENT':
-                return state - 1;
-
-            default:
-                return state;
-        }
+(function () {
+  // Reducer
+  //  (state, action) => state
+  //      INCREMENT ? state + 1
+  //      DECREMENT ? state - 1
+  var counterReducer = function (state, action) {
+    // INITIAL STATE
+    if (state === undefined) {
+      return 0
     }
 
-    // Store
-    //  createStore(reducer)
-    //  subscribe to change
-    var store = Redux.createStore(counterReducer);
+    // return new state based on actions
+    switch (action.type) {
+      case 'INCREMENT':
+        return state + 1
 
-    // Action Creators
-    //  INCREMENT
-    //  DECREMENT
-    var increment = function(){
-        return {type: 'INCREMENT'}
+      case 'DECREMENT':
+        return state - 1
+
+      default:
+        return state
     }
-    var decrement = function(){
-        return {type: 'DECREMENT'}
+  }
+
+  // Store
+  //  createStore(reducer)
+  //  subscribe to change
+  var store = Redux.createStore(counterReducer)
+
+  // Action Creators
+  //  INCREMENT
+  //  DECREMENT
+  var increment = function () {
+    return {
+      type: 'INCREMENT'
     }
-
-    // View
-    var updateView = function(){
-        var count = store.getState().toString();
-        document.getElementById("count").innerHTML = count;
+  }
+  var decrement = function () {
+    return {
+      type: 'DECREMENT'
     }
-    store.subscribe(updateView)
+  }
 
-    // View Actions
-    document.getElementById("increment")
-        .addEventListener("click", function(){
-            store.dispatch(increment())
-        })
+  // View
+  var updateView = function () {
+    var count = store.getState().toString()
+    document.getElementById(`count`).innerHTML = count
+  }
+  store.subscribe(updateView)
 
-    document.getElementById("decrement")
-        .addEventListener("click", function(){
-            store.dispatch(decrement())
-        })
-    
-})()
+  // View Actions
+  document.getElementById(`increment`)
+    .addEventListener(`click`, function () {
+      store.dispatch(increment())
+    })
+
+  document.getElementById(`decrement`)
+    .addEventListener(`click`, function () {
+      store.dispatch(decrement())
+    })
+}())
