88e2aeefa55177bb48e549aba89be3cae94156fc
Dale 'Ducky' Lotts <dlotts@knightrider.com>
2017-05-11 10:22:01 -0500

    style(vanilla): fix white space

--- Redux/REF_Vanilla/main.js
+++ Redux/REF_Vanilla/main.js
@@ -1,63 +1,61 @@
-(function () {
-  // Reducer
-  //  (state, action) => state
-  //      INCREMENT ? state + 1
-  //      DECREMENT ? state - 1
-  var counterReducer = function (state, action) {
-    // INITIAL STATE
-    if (state === undefined) {
-      return 0;
-    }
+;(function(){
 
-    // return new state based on actions
-    switch (action.type) {
-      case 'INCREMENT':
-        return state + 1;
+    // Reducer
+    //  (state, action) => state
+    //      INCREMENT ? state + 1
+    //      DECREMENT ? state - 1
+    var counterReducer = function(state, action) {
+        // INITIAL STATE
+        if(state === undefined){
+            return 0;
+        }
 
-      case 'DECREMENT':
-        return state - 1;
+        // return new state based on actions
+        switch(action.type) {
+            case 'INCREMENT':
+                return state + 1;
+            
+            case 'DECREMENT':
+                return state - 1;
 
-      default:
-        return state;
-    }
-  };
+            default:
+                return state;
+        }
+    };
 
-  // Store
-  //  createStore(reducer)
-  //  subscribe to change
-  var store = Redux.createStore(counterReducer)
+    // Store
+    //  createStore(reducer)
+    //  subscribe to change
+    var store = Redux.createStore(counterReducer);
 
-  // Action Creators
-  //  INCREMENT
-  //  DECREMENT
-  var increment = function () {
-    return {
-      type: 'INCREMENT'
-    }
-  };
+    // Action Creators
+    //  INCREMENT
+    //  DECREMENT
+    var increment = function(){
+        return {type: 'INCREMENT'}
+    };
+    
+    var decrement = function(){
+        return {type: 'DECREMENT'}
+    };
 
-  var decrement = function () {
-    return {
-      type: 'DECREMENT'
-    }
-  };
+    // View
+    var updateView = function(){
+        var count = store.getState().toString();
+        document.getElementById('count').innerHTML = count;
+    };
+    
+    store.subscribe(updateView)
 
-  // View
-  var updateView = function () {
-    var count = store.getState().toString()
-    document.getElementById('count').innerHTML = count
-  };
+    // View Actions
+    document.getElementById('increment')
+        .addEventListener('click', function(){
+            store.dispatch(increment())
+        });
 
-  store.subscribe(updateView);
-
-  // View Actions
-  document.getElementById('increment')
-    .addEventListener('click', function () {
-      store.dispatch(increment())
-    });
-
-  document.getElementById('decrement')
-    .addEventListener('click', function () {
-      store.dispatch(decrement())
-    })
+    document.getElementById('decrement')
+        .addEventListener('click', function(){
+            store.dispatch(decrement())
+        });
+    
 }());
